8756dad0fd4fc54b559e889879112c68cf2d6b2a
Elvis <elvinasu@gmail.com>
2021-10-25 11:24:29 +0300

    utils

--- src/API/v1/users.js
+++ src/API/v1/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const mysql = require(`mysql2/promise`)
+const {hashValue} = require('../../utils/hashHelper');
+const {validateRegister} = require('../../utils/validateHelper');
+// const mysql = require(`mysql2/promise`)
 
-const {dConfig} = require('../../config')
+
+// const {dConfig} = require('../../config')
 
 const router = express.Router();
 // POST /users/register - create new user
-router.post('/register', async (req, res)=> {
-    res.json('register user here')
+router.post('/register',validateRegister, async (req, res)=> {
+    const newUser = {
+        full_name: req.body.full_name,
+        email: req.body.email,
+        password: hashValue(req.body.password),
+    };
+
+    res.json({msg:'register user here', newUser});
+
 })
 
 // validation
