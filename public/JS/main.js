cd84be8d55353a26634910235042de581732d74d
Elvis <elvinasu@gmail.com>
2021-10-26 11:51:33 +0300

    showing all groups from fetch

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,21 +1,48 @@
 console.log('front');
 const URL = 'http://localhost:3000/groups';
 
+// elements
+const groupsContainer = document.querySelector('.groups-container')
+
 // fetch all groups
-async function fetchData(){
-    const resp = await fetch(`${URL}/all`);
+async function fetchData(urlPath){
+    const resp = await fetch(`${URL}${urlPath}`);
     const dataFromResp = await  resp.json();
     return dataFromResp
 }
 async function init(){
-    getGroups();
+    const allGroupsArr = await getGroups() ;
+    // console.log(allGroupsArr)
+    generateGroups(allGroupsArr,groupsContainer)
 
 }
 init();
 
 async function getGroups(){
 
-    const data = await fetchData()
-    console.log('data', data)
+    const data = await fetchData('/all')
+    // console.log('data', data)
+    if(data.msg === 'success'){
+        return data.data;
+    }
+    throw new Error('no groups found');
 
 }
+
+// show group cards
+
+function generateGroups (dataArr, dest){
+    dest.innerHTML = dataArr.map(
+        (group) => `
+         <div class="card">
+                <h3> <a style="text-decoration: none" href="singleGroup.html">ID:${group.id}</a></h3>
+                <p> <a style="text-decoration: none" href="singleGroup.html">${group.name}</a></p>
+
+            </div>
+        
+        `,
+
+    ).join('')
+
+}
+
